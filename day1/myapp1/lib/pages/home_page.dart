d4d278be81ae063fbdc52469553bcdf79a3d76e5
Debidattaray <debidattaray@gmail.com>
2021-02-16 18:09:48 +0530

    json mapping

--- day1/myapp1/lib/pages/home_page.dart
+++ day1/myapp1/lib/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:myapp1/models/catalog.dart';
 import 'package:myapp1/models/catalog.dart';
 import 'package:myapp1/widgets/drawer.dart';
 import 'package:myapp1/widgets/item_widget.dart';
+import 'dart:convert';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogjson);
+    var productData = decodedData["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.item[0]);
